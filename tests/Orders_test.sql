e220460b473245ebc01b09844a1f337125499786
Andy Debney <andy.debney@dbtlabs.com>
2026-08-11 21:19:00 +0000

    Test 11

--- tests/Orders_test.sql
+++
@@ -1,6 +0,0 @@
-select
-   orders.order_id
-from {{ ref('fct_orders') }} as orders
-left join {{ ref('fct_order_items') }} as order_items
-   on orders.order_id = order_items.order_id
-where order_items.order_id is null
